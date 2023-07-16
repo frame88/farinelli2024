776bdf7a27f565d1306bc8b67b5bfea7feac0c01
frame88 <fleuzzi97@gmail.com>
2023-07-16 14:44:43 +0200

    importazione libreria, e html base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fullpagedacapo';
+
+  config: any;
+  fullpage_api: any;
+
+    constructor() {
+
+    // for more details on config options please visit fullPage.js docs
+    this.config = {
+
+      // fullpage options
+      licenseKey: '5L8DI-V6OKJ-HNS0H-5P2G7-YNYPN',
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      menu: '#menu',
+
+      // fullpage callbacks
+      afterResize: () => {
+        console.log("After resize");
+      },
+      afterLoad: (origin: { index: any; }, destination: any, direction: any) => {
+        console.log(origin.index);
+      }
+    };
+  }
+
+  getRef(fullPageRef: any) {
+    this.fullpage_api = fullPageRef;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
